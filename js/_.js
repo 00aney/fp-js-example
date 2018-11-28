3bef07a972e270c3ba65bb70fe618486b38c0875
Ted Kim <yenafirst91@gmail.com>
2018-11-28 16:16:55 -0500

    Update _.js

--- js/_.js
+++ js/_.js
@@ -1,23 +1,49 @@
-const _each = (list, iter) {
-  for (let i = 0; i < list.length; i++) {
-    iter(list[i]);
+function _curry(fn) {
+  return function(a, b) {
+    return arguments.length == 2 ? fn(a, b) : function(b) { return fn(a, b); };
+  }
+}
+
+function _curryr(fn) {
+  return function(a, b) {
+    return arguments.length == 2? fn(a, b) : function(b) { return fn(b, a); };
+  }
+}
+
+var _get = _curryr(function(obj,key) {
+  return obj == null ? undefined : obj[key];
+});
+
+function _filter(list, predi) {
+  var new_list = [];
+  _each(list, function(val) {
+    if(predi(val)) new_list.push(val);
+  });
+  return new_list;
+}
+
+function _map(list, mapper) {
+  var new_list = [];
+  _each(list, function(val, key) {
+    new_list.push(mapper(val, key));
+  });
+  return new_list;
+}
+
+function _is_object(obj) {
+  return typeof obj == 'object' && !!obj;
+}
+
+function _keys(obj) {
+  return _is_object(obj) ? Object.keys(obj) : [];
+}
+
+var _length = _get('length');
+
+function _each(list, iter) {
+  var keys = _keys(list);
+  for (var i = 0, len = keys.length; i < len; i++) {
+    iter(list[keys[i]], keys[i]);
   }
   return list;
 }
-
-const _filter = (list, predi) => {
-  let new_list = [];
-  _each(list, (item) => {
-    if (predi(item)) new_list.push(item);
-  })
-  return new_list;
-}
-
-const _map = (list, mapper) => {
-  let new_list = [];
-  _each(list, (item) => {
-    new_list.push(mapper(item));
-  })
-  return new_list;
-}
-
